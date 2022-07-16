5e3062df045438e1f25dcec823d48c5ec0f61408
shivamkshirsagar11 <shivamkshirsagar2002@gmail.com>
2022-07-16 22:52:41 +0530

    add to cART UPDATED

--- health_shop/store/models.py
+++ health_shop/store/models.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 from django.db import models
-
+import secrets
+import string
 class Auth_User(models.Model):
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=15)
@@ -23,15 +24,15 @@ class Product(models.Model):
     @staticmethod
     def get_all_products():
         return Product.objects.all()
-    def get_products_by_id(category_name):
+    def get_products_by_category(category_name):
         if category_name:
             return Product.objects.filter(category=category_name)
         else:
             return Product.objects.all()
+    def get_products_by_id(id):
+        return Product.objects.filter(id=id).first()
 
 class Cart_generator(models.Model):
-    import secrets
-    import string
     user_id = models.IntegerField(default=-1)
     cart_id = models.CharField(default="to_be_assigned",max_length=100)
 
@@ -40,8 +41,8 @@ class Cart_generator(models.Model):
     @staticmethod
     def get_cart(user_id):
         return Cart_generator.objects.filter(user_id = user_id).first()
-    def generate_cart(self):
-        temp = ''.join(self.secrets.choice(self.string.ascii_uppercase + self.string.digits)
+    def generate_cart():
+        temp = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
               for i in range(7))
         return str(temp)
     def get_cart_items(cart_no):
@@ -52,8 +53,11 @@ class Cart_Item(models.Model):
     product_name = models.CharField(max_length=200)
     product_price = models.CharField(max_length=10)
     time_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self): return self.product_name+"->"+str(self.user_id)
+    total_ord = models.CharField(max_length=3,default="0")
+    total_price = models.CharField(max_length=5,default="0")
+    product_id = models.CharField(max_length=13,default="-1")
+    image = models.TextField(max_length=150,default="user_profile/default.png")
+    def __str__(self): return self.product_name+"->"+str(self.cart_no)
     # @staticmethod
     # def get_cart(id):
     #     Cart.objects.filter(user_id=id).first()
