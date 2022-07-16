35d57093af1648901ced9a7ef444118fdde1478a
adarsh162 <adarsh161202@gmail.com>
2022-07-16 11:46:33 +0530

    models created

--- health_shop/store/models.py
+++ health_shop/store/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 
 class Auth_User(models.Model):
@@ -10,3 +11,23 @@ class Auth_User(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    discription=models.CharField(max_length=100,default='',null=True,blank=True)
+    price=models.IntegerField()
+    category=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='product_details',default="user_profile/default.png")
+ 
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+    def get_products_by_id(category_name):
+        if category_name:
+            return Product.objects.filter(category=category_name)
+        else:
+            return Product.objects.all()
+
+
+
+
